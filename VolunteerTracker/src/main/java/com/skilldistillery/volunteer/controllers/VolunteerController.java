7c52c4ad65b9e0bca9904fee9a9a3b570d8083e1
paulxl <paulxlewis@me.com>
2019-09-28 20:57:08 -0600

    controller finished, now just to get application to run

--- VolunteerTracker/src/main/java/com/skilldistillery/volunteer/controllers/VolunteerController.java
+++ VolunteerTracker/src/main/java/com/skilldistillery/volunteer/controllers/VolunteerController.java
@@ -3,8 +3,10 @@ package com.skilldistillery.volunteer.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,16 +23,48 @@ public class VolunteerController {
 	public String ping() {
 	  return "pong";
 	}
+	//List<Volunteer> findAll();
 	@GetMapping("volunteer")
 	public List<Volunteer> listAllVolunteers() {
 		List<Volunteer> vols = volServ.findAll();
 		return vols;	
 	}
+	//Volunteer findById(int id);
 	@GetMapping("volunteer/{volId}")
 	public Volunteer getById(@PathVariable("volId") int id) {
 		return volServ.findById(id);
 		
 	}
+	//Volunteer findByUserName(String username);
+	@GetMapping("volunteer/{username}")
+	public Volunteer getByUsername(@PathVariable("username") String username) {
+		return volServ.findByUserName(username);
+	}
+	//List<Volunteer> findByActive(Boolean active);
+	@GetMapping("volunteer/{true}")
+	public List<Volunteer> findActiveVolunteers(@PathVariable("true") Boolean active){
+		return volServ.findByActive(active);
+	}
+	//Volunteer updateById(int id, Volunteer volunteer);
+	@PostMapping("volunteer/{id}/volunteer")
+	public Volunteer updateById(@PathVariable("id") int id, Volunteer volunteer) {
+		return volServ.updateById(id, volunteer);
+	}
+	//Volunteer updateByUserName(String username, Volunteer volunteer);	
+	@PostMapping("volunteer/{username}/volunteer")
+	public Volunteer updateByUsername(@PathVariable("username") String username, Volunteer volunteer) {
+		return volServ.updateByUserName(username, volunteer);
+	}
+	//Volunteer deleteById(int id);
+	@DeleteMapping("volunteer/{id}")
+	public void destroyUser(@PathVariable("id") int id) {
+		 volServ.deleteById(id);
+	}
 	
+	//Volunteer createNew(Volunteer volunteer);
+	@PostMapping("volunteer/volunteer")
+	public Volunteer createVolunteer(Volunteer volunteer) {
+		return volServ.createNew(volunteer);
+	}
 
 }
