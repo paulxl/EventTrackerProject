8b8e4dea83810be836dbcf2533a1d750b043980c
paulxl <paulxlewis@me.com>
2019-10-13 13:07:52 -0600

    added bootstrap and tweaked it for appearance

--- AngularSideOfVT/src/app/app.module.ts
+++ AngularSideOfVT/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VolunteerComponent } from './component/volunteer/volunteer.component';
 import { FormsModule } from '@angular/forms';
-
+//import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -18,6 +18,7 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     HttpClientModule
+    // NgbModule.forRoot(),
   ],
   providers: [
     VolunteerService
